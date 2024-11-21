26d5da32a5206965d213cc65e7f7a2ec61791485
vishalgavali193 <vishalgavali2004@gmail.com>
2024-11-22 03:33:55 +0530

    Add files via upload

---
+++ sudoku.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+#define N 9
+bool isSafe(int board[N][N], int row, int col, int num) {
+    for (int x = 0; x < N; x++) {
+        if (board[row][x] == num || board[x][col] == num)
+            return false;
+    }
+    int startRow = row - row % 3, startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i + startRow][j + startCol] == num)
+                return false;
+        }
+    }
+    return true;
+}
+
+bool solveSudoku(int board[N][N]) {
+    int row, col;
+    bool empty = true;
+    for (row = 0; row < N; row++) {
+        for (col = 0; col < N; col++) {
+            if (board[row][col] == 0) {
+                empty = false;
+                break;
+            }
+        }
+        if (!empty)
+            break;
+    }
+    if (empty)
+        return true;
+    for (int num = 1; num <= 9; num++) {
+        if (isSafe(board, row, col, num)) {
+            board[row][col] = num;
+            if (solveSudoku(board))
+                return true;
+            board[row][col] = 0;
+        }
+    }
+    return false;
+}
+void printBoard(int board[N][N]) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            cout << board[row][col] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int board[N][N] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+    
+    if (solveSudoku(board))
+        printBoard(board);
+    else
+        cout << "No soln exist";
+    return 0;
+}
